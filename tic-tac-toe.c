b0bb5f8798727ddfda2172ff2d6f845556725b13
Maung945 <myoaung@cpp.edu>
2022-10-06 00:35:41 -0700

    Display Board and P1 P2 moves

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,5 +1,6 @@
 // Tick Tac Toe game between 2 players or player vs computer
 #include <stdio.h>
+int board[3][3];
 void playerVsPlayer(int x) {
 
 }
@@ -8,26 +9,93 @@ void plyerVsComputer(int x) {
 
 }
 
+void setSymbol(int row, int column) {
+    int symbol = (int)'&';
+    board[row][column] = symbol;
+}
+
 void displayBoard() {
     printf("+-----+\n");
-    int board[3][3];
-    for(int i = 0; i < 3; i++) {
-       for(int j = 0; j < 3; j++) {
-            printf("| ");
+    for(int i = 1; i <= 3; i++) {
+        printf("|");
+        for(int j = 1; j <= 3; j++) {
+            board[i][j] = ' ';
+            printf("%c", board[i][j]);
+            printf("|");
         }
-        printf("|\n");
+        printf("\n");
         printf("+-----+\n");
     }
-    //printf("+------------+\n");
+}
+void playerOneMove() {
+    int row, column, symbol;
+    printf("Player1 : make a move\n");
+    scanf("%d %d", &row, &column);
+    if(row < 1 || row > 3 || column < 1 || column > 3) {
+        printf("Invalid Entry, Try Again!\n");
+        playerOneMove();
+        return;
+    }
+    if(board[row][column] == 'X') {
+        printf("Invalid Move!\n");
+        playerOneMove();
+        return;
+    }
+    printf("+-----+\n");
+    for(int i = 1; i <= 3; i++) {
+        printf("|");
+        for(int j = 1; j <= 3; j++) {
+            if(i == row && j == column && board[i][j] != 'X') {
+                board[i][j] = 'O';
+            }
+            printf("%c", board[i][j]);
+            printf("|");
+        }
+        printf("\n");
+        printf("+-----+\n");
+    }
+}
+
+void playerTwoMove() {
+    int row, column, symbol;
+    printf("Player2 : make a move\n");
+    scanf("%d %d", &row, &column);
+    if(row < 1 || row > 3 && column < 1 || column > 3) {
+        printf("Invalid Entry, Try Again!\n");
+        playerOneMove();
+        return;
+    }
+    if(board[row][column] == 'O') {
+        printf("Invalid Move!\n");
+        playerTwoMove();
+        return;
+    }
+    printf("+-----+\n");
+    for(int i = 1; i <= 3; i++) {
+        printf("|");
+        for(int j = 1; j <= 3; j++) {
+            if(i == row && j == column) {
+                board[i][j] = 'X';
+            }
+            printf("%c", board[i][j]);
+            printf("|");
+        }
+        printf("\n");
+        printf("+-----+\n");
+    }
 }
 
 int main() {
     // prompt user for game they wish to play
+    int choice;
+    int row = 3;
+    int column = 3;
+    int symbol = (int) '.';
     printf("   WELCOME TO TIC-TAC-TOE    \n");
     printf("1 --- person vs. person\n");
     printf("2 --- person vs. random computer\n");
   
-    int choice;
+    
     printf("Enter your choice (1 or 2): ");
     scanf("%d", &choice);
     switch(choice) {
@@ -35,11 +103,15 @@ int main() {
             printf("You have entered choice 1: \n");
             printf("==========================\n");
             displayBoard();
+            playerOneMove();
+            playerTwoMove();
             break;
         case 2:
             printf("You have entered choide 2: \n");
             printf("==========================\n");
             displayBoard();
+            playerOneMove();
+            playerTwoMove();
             break;
         default:
             printf("Enter a valid choice\n");
