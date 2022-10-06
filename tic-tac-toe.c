8b06df90d02aea014dd785bf397d954776f4d15d
Maung945 <myoaung@cpp.edu>
2022-10-05 19:35:19 -0700

    Adding switch statements

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,14 +1,26 @@
 // Tick Tac Toe game between 2 players or player vs computer
-
+#include <stdio.h>
 int main() {
     // prompt user for game they wish to play
-
-
+    printf("   WELCOME TO TIC-TAC-TOE    \n");
+    printf("1 --- person vs. person\n");
+    printf("2 --- person vs. random computer\n");
+  
+    int choice;
+    printf("Enter a choice now: ");
+    scanf("%d", &choice);
+    switch(choice) {
+        case 1:
+            printf("person to person\n");
+            break;
+        case 2:
+            printf("Person vs computer\n");
+            break;
+        default:
+            printf("Enter a valid choice\n");
+    }
 
     // read information console
-
-
-    
 
 
     return 0;
