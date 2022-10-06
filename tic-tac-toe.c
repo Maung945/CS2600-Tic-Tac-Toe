f15115d185788cbadb7283443f9c0c0a3d159868
Maung945 <myoaung@cpp.edu>
2022-10-05 19:19:22 -0700

    First Commit

---
+++ tic-tac-toe.c
@@ -0,0 +1,15 @@
+// Tick Tac Toe game between 2 players or player vs computer
+
+int main() {
+    // prompt user for game they wish to play
+
+
+
+    // read information console
+
+
+    
+
+
+    return 0;
+}
